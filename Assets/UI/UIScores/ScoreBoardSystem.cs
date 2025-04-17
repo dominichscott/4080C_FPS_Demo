b83827a3c7d0997a4ada709f8f4c38c4591218be
Dominic <dominichenryscott@gmail.com>
2025-04-17 16:49:08 -0400

    Scoreboard and Chat fixes

--- Assets/UI/UIScores/ScoreBoardSystem.cs
+++ Assets/UI/UIScores/ScoreBoardSystem.cs
@@ -14,7 +14,7 @@ partial class ScoreBoardSystem : SystemBase
     private GameObject uiObject;
 
     PlayerNameManager pNameManager;
-
+    private int netId = 999;
     protected override void OnCreate()
     {
         ghostQuery = GetEntityQuery(typeof(GhostInstance), typeof(HealthComponent));
@@ -26,6 +26,22 @@ partial class ScoreBoardSystem : SystemBase
 
     protected override void OnUpdate()
     {
+        EntityQuery connectionQuery = GetEntityQuery(
+            ComponentType.ReadOnly<NetworkStreamInGame>(),
+                ComponentType.ReadOnly<NetworkId>()
+        );
+        if (connectionQuery.IsEmpty)
+        {
+            Debug.Log("No conn");
+        }
+        // netId hasn't been changed yet
+        else if (netId > 10)
+        {
+            var connectionEntity = connectionQuery.GetSingletonEntity();
+            netId = EntityManager.GetChunkComponentData<NetworkId>(connectionEntity).Value;
+            // connected debug code could go here
+        }
+        
         if (uiObject == null)
         {
             // Debug.LogWarning("No UIManager, searching");
@@ -77,6 +93,11 @@ partial class ScoreBoardSystem : SystemBase
         Entities.ForEach((ref HealthComponent healthComp) =>
         //.WithAll<GhostInstance>().ForEach((ref HealthComponent health, ref GhostOwner ghostOwner, ref GhostOwnerIsLocal gol) =>
         {
+            if (netId == healthComp.ownerNetworkID)
+            {
+                healthComp.playerName = pNameManager.GetPlayerName();
+            }
+                
             healthComp.playerName = pNameManager.GetPlayerName();
             cnt++;
             PlayerScore tmp = new PlayerScore("" + healthComp.playerName, (int)healthComp.kills, (int)healthComp.deaths);
